b2bc804887e5693641541941521ba2bde8db7577
JoAn <joannacalinska@gmail.com>
2019-11-01 20:42:10 +0100

    fix interface RequestWithBody

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 
-interface RequestWithBody {
+interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
@@ -24,12 +24,6 @@ router.get('/login', (req: Request, res: Response) => {
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-
-  if (email) {
-    res.send(email.toUpperCase());
-  } else {
-    res.send('you must provide an email');
-  }
 });
 
 export { router };
